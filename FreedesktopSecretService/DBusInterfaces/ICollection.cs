24e1e86286a03f51f303bba11a1f0a18e8e26771
ftsell <contact@finn-thorben.me>
2018-12-11 14:00:21 +0100

    Define Secret.Collection interface

--- FreedesktopSecretService/DBusInterfaces/ICollection.cs
+++ FreedesktopSecretService/DBusInterfaces/ICollection.cs
@@ -1,7 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using FreedesktopSecretService.DBusInterfaces;
+using Tmds.DBus;
+
 namespace FreedesktopSecretService
 {
-    public interface ICollection
+    public interface ICollection : IDBusObject
     {
+
+        //
+        // Methods
+        //
+        Task<ObjectPath> Delete();
+
+        Task<ObjectPath[]> SearchItems(IDictionary<string, string> attributes);
+
+        Task<(ObjectPath item, ObjectPath prompt)> CreateItem(IDictionary<string, object> properties, Secret secret, bool replace);
         
+        //
+        // Signals
+        //
+        Task<IDisposable> WatchItemCreatedAsync(Action<ObjectPath> handler);
+        
+        Task<IDisposable> WatchItemDeletedAsync(Action<ObjectPath> handler);
+        
+        Task<IDisposable> WatchItemChangedAsync(Action<ObjectPath> handler);
+        
+        //
+        // Properties
+        //
+
     }
 }
